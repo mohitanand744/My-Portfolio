04a0ddb2cef28e745e5e2c14de30f1194954e1b3
Mohit Jha <95844984+mohitanand744@users.noreply.github.com>
2024-04-21 21:10:41 +0530

    added menu button for responsive navbar

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
-let readMoreTxt = document.querySelector(".read-more-txt");
-let readMoreBtn = document.querySelector(".read-more-btn");
+let readMoreBtn = document.querySelectorAll(".read-more-btn");
+let dropDownBtn = document.querySelector(".menu");
 
+let dropDownValue = false;
 let rMoreBtn = false;
 
-// ! Click Events
 
-readMoreBtn.addEventListener("click", () => {
+
+
+// ! ReadMore Function
+
+let readMore = (event) => {
+
+    let readMoreTxt = event.target.previousElementSibling;
+
     if (!rMoreBtn) {
         readMoreTxt.style.display = "inline";
-        readMoreBtn.innerText = "...Read less"
+        event.target.innerText = "...Read less"
 
         rMoreBtn = true;
     } else {
         readMoreTxt.style.display = "none";
-        readMoreBtn.innerText = "...Read more";
+        event.target.innerText = "...Read more";
         rMoreBtn = false;
     }
+}
 
 
+// ! dropDown function
+
+let dropDown = () => {
+    if (!dropDownValue) {
+        document.querySelector(".dropdown-menu").style.height = "310px";
+        document.querySelector(".dropdown-menu").style.display = "flex";
+        dropDownValue = true;
+    } else {
+        document.querySelector(".dropdown-menu").style.height = "0";
+        dropDownValue = false;
+    }
+}
+
+
+//! Loop
+
+readMoreBtn.forEach((currElem) => {
+
+    // ! Click Events
+    currElem.addEventListener("click", readMore)
 })
+
+// ! click Event 
+
+dropDownBtn.addEventListener("click", dropDown);
