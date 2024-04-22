d443dbd83a166d44182f6089cbcecdfe55b1065a
Mohit Jha <95844984+mohitanand744@users.noreply.github.com>
2024-04-22 10:57:58 +0530

    Added few functionalities using javascript

--- script.js
+++ script.js
@@ -1,53 +1,66 @@
 let readMoreBtn = document.querySelectorAll(".read-more-btn");
 let dropDownBtn = document.querySelector(".menu");
-
+let navLinks = document.querySelectorAll(".nav-links a");
+let menuLinks = document.querySelectorAll("li");
 let dropDownValue = false;
 let rMoreBtn = false;
-
-
-
 
 // ! ReadMore Function
 
 let readMore = (event) => {
+  let readMoreTxt = event.target.previousElementSibling;
 
-    let readMoreTxt = event.target.previousElementSibling;
+  if (!rMoreBtn) {
+    readMoreTxt.style.display = "inline";
+    event.target.innerText = "...Read less";
 
-    if (!rMoreBtn) {
-        readMoreTxt.style.display = "inline";
-        event.target.innerText = "...Read less"
-
-        rMoreBtn = true;
-    } else {
-        readMoreTxt.style.display = "none";
-        event.target.innerText = "...Read more";
-        rMoreBtn = false;
-    }
-}
-
+    rMoreBtn = true;
+  } else {
+    readMoreTxt.style.display = "none";
+    event.target.innerText = "...Read more";
+    rMoreBtn = false;
+  }
+};
 
 // ! dropDown function
 
 let dropDown = () => {
-    if (!dropDownValue) {
-        document.querySelector(".dropdown-menu").style.height = "310px";
-        document.querySelector(".dropdown-menu").style.display = "flex";
-        dropDownValue = true;
-    } else {
-        document.querySelector(".dropdown-menu").style.height = "0";
-        dropDownValue = false;
-    }
-}
+  if (!dropDownValue) {
+    document.querySelector(".dropdown-menu").style.height = "310px";
+    document.querySelector(".dropdown-menu").style.display = "flex";
+    dropDownValue = true;
+  } else {
+    document.querySelector(".dropdown-menu").style.height = "0";
+    dropDownValue = false;
+  }
+};
 
+//! Active Class Function
 
-//! Loop
+let activeClass = (e) => {
+  let activeElement = document.querySelector(".active");
+  if (activeElement) {
+    activeElement.classList.remove("active");
+  }
+  e.target.classList.add("active");
+};
+
+//!  Iteration
 
 readMoreBtn.forEach((currElem) => {
+  // ! Click Events
+  currElem.addEventListener("click", readMore);
+});
 
-    // ! Click Events
-    currElem.addEventListener("click", readMore)
-})
+navLinks.forEach((currElema) => {
+  currElema.addEventListener("click", activeClass);
+});
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener("click", () => {
+    document.querySelector(".dropdown-menu").style.height = "0";
+  });
+});
 
-// ! click Event 
+// ! click Event
 
 dropDownBtn.addEventListener("click", dropDown);
